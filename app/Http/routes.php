d1e3bbc75badf1389364888d0c9b894014c4e50f
albertbaldi <albertbaldi@gmail.com>
2015-09-13 23:48:19 -0300

    Products model, migration and controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+
+	/**
+	 * Produtos
+	 */
+	Route::group(['prefix' => 'produtos', 'as' => 'product.'], function () {
+
+		Route::get('', ['as' => 'index', 'uses' => 'ProductController@index']);
+		Route::get('novo', ['as' => 'create', 'uses' => 'ProductController@create']);
+		Route::post('incluir/{id}', ['as' => 'store', 'uses' => 'ProductController@store']);
+		Route::get('editar/{id}', ['as' => 'edit', 'uses' => 'ProductController@edit']);
+		Route::post('atualizar/{id}', ['as' => 'update', 'uses' => 'ProductController@update']);
+		Route::get('excluir/{id}', ['as' => 'destroy', 'uses' => 'ProductController@destroy']);
+
+	});
+
+	
+
 });
